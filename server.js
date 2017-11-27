aaf2706e5bb186bdcf770d8204db40a831f38a96
therick96 <erick25gomez@gmail.com>
2017-11-27 16:31:20 -0400

    Documentacion de codigo

---
+++ server.js
@@ -0,0 +1,8 @@
+var http = require("http"); 
+/*
+filesystem: permite controlar cosas en el sistema local
+http: Para crear servidores de aplicaciones web
+events: Para crear administradores de eventos
+querystring: Para parsear una cadena a un objeto javascript
+path: Permite manipular ruta de los archivos de nuestro sistema local
+*/
