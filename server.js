f2b66bb58e96668718fb0b9a84cdf78b65902b9a
therick96 <erick25gomez@gmail.com>
2017-11-27 16:35:22 -0400

    Creado holamundo

--- server.js
+++ server.js
@@ -6,3 +6,7 @@ events: Para crear administradores de eventos
 querystring: Para parsear una cadena a un objeto javascript
 path: Permite manipular ruta de los archivos de nuestro sistema local
 */
+var server = http.createServer(function(req, res){
+    res.writeHead("200", {'content-type' : 'text/plain'});
+    res.end("Hola Mundo");
+}).listen("3000");
