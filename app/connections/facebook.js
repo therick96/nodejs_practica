8f4950377cf604d3991aa28ad67ca67d79a87151
therick96 <erick25gomez@gmail.com>
2017-12-01 14:47:52 -0400

    Añadida conexion con facebook para autentificacion

--- app/connections/facebook.js
+++ app/connections/facebook.js
@@ -1,5 +1,24 @@
+var passport = require('passport'),
+    FacebookStrategy = require('passport-facebook').Strategy; //Estrategia de auentificacion
+
 var facebookConection = function (server){
     console.log('Conexion con facebook');
+    passport.use(
+        new FacebookStrategy(
+            {
+                clientID : '1659775674085399',
+                clientSecret : '31f006715e3dc78d0c06be22ea8dec8f',
+                callbackURL : 'http://localhost:8000/auth/facebook/callback' //Url donde regresara despues de autentificar
+            }, 
+            function(accessToken, RefreshToken, profile, done){
+                done(null, profile); //Profile son todos los datos de la persona que se logea con facebook
+            }
+        )
+    );
+    server.get('/auth/facebook', passport.authenticate('facebook')); //Para autentificarme con fb es necesario ir a esa url
+    server.get('/auth/facebook/callback', passport.authenticate('facebook', {
+        successRedirect : '/', failureRedirect : '/errores'}));
+
 };
 
 module.exports = facebookConection;
--- app/controllers/home.js
+++ app/controllers/home.js
@@ -4,6 +4,11 @@ var homeController = function (server){
     server.route('/')
         .get(function(req, res){
             //res.send('Hola Mundo');
+            if (req.user){//Si se logueo
+                var nombre = req.user._json.first_name; //Trae el nombre
+                var url_foto = 'http://graph.facebook.com/' + req.user.id + '/picture'; //Trae la url de la foto
+                console.log(nombre + ' ' + url_foto);
+            }
             res.render('home/index');
         });
 };
