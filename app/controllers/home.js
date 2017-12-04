e2317a16f6e56bd03a305b39a1e1ee41daab6894
therick96 <erick25gomez@gmail.com>
2017-12-04 14:19:01 -0400

    loging con facebook

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -5,11 +5,13 @@ var homeController = function (server){
         .get(function(req, res){
             //res.send('Hola Mundo');
             if (req.user){//Si se logueo
-                var nombre = req.user._json.first_name; //Trae el nombre
+                var nombre = req.user._json.name; //Trae el nombre
                 var url_foto = 'http://graph.facebook.com/' + req.user.id + '/picture'; //Trae la url de la foto
                 console.log(nombre + ' ' + url_foto);
+                res.render('home/index', {foto : url_foto, usuario : nombre});
+            }else{
+                res.render('base');
             }
-            res.render('home/index');
         });
 };
 
